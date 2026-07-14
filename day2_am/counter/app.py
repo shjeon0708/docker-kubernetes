82ae79e7471e309886afa47ea7a6321e7f664baf
shjeon0708 <94080990+shjeon0708@users.noreply.github.com>
2026-07-14 09:01:42 +0900

    Create app.py

---
+++ day2_am/counter/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import redis
+
+app = Flask(__name__)
+r = redis.Redis(
+  host='redis',
+  port=6379)
+
+@app.route('/')
+def count():
+  n = r.incr('hits')
+  return f'방문 {n}회'
